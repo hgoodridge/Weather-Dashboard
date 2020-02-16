7907421aee1db0f3532dc40c94ed861f45ca716a
Hiram <hiramgoodridge@gmail.com>
2020-02-16 11:26:53 -0500

    added image icon beside city title

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,21 +2,28 @@ $("#searchBtn").on("click",function(){
     console.log($("#citySearch").val())
     
     var searchResult = $("#citySearch").val()
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + searchResult + "&appid=2b8f9155e88bbf2c5d51baf0c1a74a68"
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + searchResult + "&units=imperial&appid=2b8f9155e88bbf2c5d51baf0c1a74a68"
     $.ajax({
         url:url,
         method:"GET"
     })
     .then(function(response){
         console.log(response)
+        // "http://openweathermap.org/img/wn/"+ icon+"@2x.png"
         var icon = (response.weather[0].icon)
-        var cityName = response.name //+
-        var temperature ="Temperature: "  + Math.round((response.main.temp - 273.15) * 1.80 + 32) + "°F";
-        var humid = "Humidity: " + response.main.humidity +"%"
-        var windSpeed = "Wind Speed: " + response.wind.speed + " Mph"
-        console.log(humid)
-       var jake = $(".cityHolder").append($("<div>").attr("class", "cityHolder"))
-       jake.text(cityName)
+        var cityName = response.name + " (" + moment().format('l') + ")";
+        var temperature = $("<div>").text("Temperature: "  + Math.round(response.main.temp) + "°F");
+        var humid = $("<div>").text("Humidity: " + response.main.humidity +"%")
+        var windSpeed = $("<div>").text("Wind Speed: " + response.wind.speed + " Mph")
+        var image = $("<img>").attr('src',"http://openweathermap.org/img/wn/" + icon + "@2x.png")
+        var main = $("<div>")
+        var space = $("<br>")
+        main.text(cityName)
+        main.append(image, temperature, humid, windSpeed)
+        var jake = $(".cityHolder").append(main)
+
+       
+    
         
         
         
